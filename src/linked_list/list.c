f83d88d3bb5b59a19c0bd084ca18065b808a5c21
Michael Sholkov <ecoaw1@gmail.com>
2021-04-21 23:42:57 +0300

    linked list release

---
+++ src/linked_list/list.c
@@ -0,0 +1,97 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "list.h"
+
+void int_display(node *head)
+{
+	for (node *ptr = head; ptr; ptr = ptr->next)
+		printf("%d ", *(int*)(ptr->data));
+	printf("\n");
+}
+
+void float_display(node *head)
+{
+	for (node *ptr = head; ptr; ptr = ptr->next)
+		printf("%f ", *(float*)(ptr->data));
+	printf("\n");
+}
+
+unsigned int list_size(node *head)
+{
+	unsigned int count = 0;
+    for (node *ptr = head; ptr; ptr = ptr->next)
+        count++;
+
+    return count;
+}
+
+void free_list(node *head)
+{
+    node *ptr = head;
+    while (ptr)
+    {
+        node *temp = ptr;
+        ptr = ptr->next;
+		free(temp->data);
+        free(temp);
+    }
+}
+
+void convert_to_circular(node *head)
+{
+	node *ptr = head;
+	while (ptr->next != NULL)
+		ptr = ptr->next;
+	
+	ptr->next = head;
+}
+
+void c_int_display(node *head)
+{
+	node *ptr = head;
+    do
+    {
+        printf("%d ", *(int*)ptr->data);
+        ptr = ptr->next;
+    } while (ptr != head);
+
+    printf("\n");
+}
+
+void c_float_display(node *head)
+{
+	node *ptr = head;
+    do
+    {
+        printf("%f ", *(float*)ptr->data);
+        ptr = ptr->next;
+    } while (ptr != head);
+
+    printf("\n");
+}
+
+void c_free_list(node *head)
+{
+	node *ptr = head;
+    do
+    {
+        node *temp = ptr;
+        ptr = ptr->next;
+		free(temp->data);
+        free(temp);
+    } while (ptr != head);
+}
+
+unsigned int c_list_size(node *head)
+{
+	unsigned int count = 0;
+
+    node *ptr = head;
+    do
+    {
+        count++;
+        ptr = ptr->next;
+    } while (ptr != head);
+    
+    return count;
+}
---
+++ src/linked_list/list.h
@@ -0,0 +1,141 @@
+#ifndef LIST_H
+#define LIST_H
+
+typedef struct node
+{
+	struct node *next;
+	void *data;
+}node, list;
+
+/* linked list */
+void int_display(node *head); //test
+void float_display(node *head); // test
+unsigned int list_size(node *head);
+void free_list(node *head);
+
+#define PUSH_FRONT_INIT(TYPE) 							\
+	node* TYPE ##_push_front(node *head, TYPE value) 	\
+	{ 													\
+		node *new_node = malloc(sizeof(node)); 			\
+		new_node->data = malloc(sizeof(TYPE)); 			\
+		*(TYPE*)new_node->data = value; 				\
+		new_node->next = head; 							\
+		head = new_node;								\
+		return head;									\
+	} 													\
+
+#define PUSH_BACK_INIT(TYPE) 							\
+	node* TYPE ##_push_back(node *head, TYPE value)	 	\
+	{ 													\
+		node *new_node = malloc(sizeof(node));		 	\
+		new_node->data = malloc(sizeof(value)); 		\
+		*(TYPE*)(new_node->data) = value; 				\
+		new_node->next = NULL; 							\
+														\
+		if (head == NULL) 								\
+			return new_node;			 				\
+														\
+		node *ptr = head;								\
+		while (ptr->next != NULL) 						\
+			ptr = ptr->next; 							\
+		ptr->next = new_node; 							\
+														\
+		return head; 									\
+	} 													\
+
+#define ERASE_INIT(TYPE) 								\
+	node* TYPE ##_erase(node *head, TYPE value) 		\
+	{ 													\
+		if (*(TYPE*)head->data == value) 				\
+		{ 												\
+			node *ptr = head; 							\
+			head = head->next; 							\
+			free(ptr->data); 							\
+			free(ptr); 									\
+			return head; 								\
+		} 												\
+														\
+		node *ptr = head, *prev; 						\
+    	while (*(TYPE*)ptr->data != value) 				\
+    	{ 												\
+        	prev = ptr; 								\
+        	ptr = ptr->next; 							\
+    	} 												\
+    	prev->next = ptr->next; 						\
+		free(ptr->data); 								\
+    	free(ptr); 										\
+														\
+    	return head; 									\
+	} 													\
+/* linked list */
+
+/* circular linked list */
+void convert_to_circular(node *head);
+void c_int_display(node *head); //test
+void c_float_display(node *head); // test
+unsigned int c_list_size(node *head);
+void c_free_list(node *head);
+
+#define C_PUSH_FRONT_INIT(TYPE) 						\
+	node* TYPE ##_c_push_front(node *head, TYPE value) 	\
+	{ 													\
+		node *new_node = malloc(sizeof(node)); 			\
+		new_node->data = malloc(sizeof(TYPE)); 			\
+    	*(TYPE*)new_node->data = value; 				\
+    	new_node->next = head; 							\
+														\
+    	node *ptr = head; 								\
+    	while (ptr->next != head) 						\
+        	ptr = ptr->next; 							\
+    	ptr->next = new_node; 							\
+														\
+    	return new_node; 								\
+	} 													\
+
+#define C_PUSH_BACK_INIT(TYPE) 							\
+	node* TYPE ##_c_push_back(node *head, TYPE value) 	\
+	{ 													\
+		node *new_node = malloc(sizeof(node)); 			\
+		new_node->data = malloc(sizeof(TYPE)); 			\
+    	*(TYPE*)new_node->data = value; 				\
+		new_node->next = head; 							\
+														\
+		node *ptr = head; 								\
+		while (ptr->next != head) 						\
+			ptr = ptr->next; 							\
+		ptr->next = new_node; 							\
+														\
+		return head; 									\
+	} 													\
+
+#define C_ERASE_INIT(TYPE) 								\
+	node* TYPE ##_c_erase(node *head, TYPE value) 		\
+	{ 													\
+		if (*(TYPE*)head->data == value) 				\
+		{ 												\
+			node *ptr = head; 							\
+			while (ptr->next != head) 					\
+				ptr = ptr->next; 						\
+			ptr->next = head->next; 					\
+			free(head->data); 							\
+			free(head); 								\
+			return ptr->next; 							\
+		} 												\
+														\
+		node *ptr = head, *prev = NULL; 				\
+		while (*(TYPE*)ptr->data != value) 				\
+		{ 												\
+			prev = ptr; 								\
+			ptr = ptr->next; 							\
+		} 												\
+		prev->next = ptr->next; 						\
+		ptr->next = prev; 								\
+		free(ptr->data); 								\
+		free(ptr); 										\
+														\
+		return head; 									\
+	} 													\
+
+/* circular linked list */
+
+#endif
