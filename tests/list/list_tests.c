d3e4bbe06151000aa20a3deef024aaab9055ca4e
Michael Sholkov <ecoaw1@gmail.com>
2021-04-22 12:43:15 +0300

    fix tabs

--- tests/list/list_tests.c
+++ tests/list/list_tests.c
@@ -4,143 +4,142 @@
 
 void test_push()
 {
-    list *l = NULL;
-
-    PUSH_FRONT_INIT(int);
-    PUSH_BACK_INIT(int);
-
-    l = int_push_front(l , 1);
-    TEST_CHECK(l != NULL);
-    TEST_CHECK(*(int*)l->data == 1);
-
-    l = int_push_back(l, 2);
-    TEST_CHECK(l != NULL);
-    TEST_CHECK(*(int*)l->next->data == 2);
-
-    free_list(l);
+        list *l = NULL;
+        PUSH_BACK_INIT(int);
+        PUSH_FRONT_INIT(int);
+    
+        l = int_push_front(l , 1);
+        TEST_CHECK(l != NULL);
+        TEST_CHECK(*(int*)l->data == 1);
+    
+        l = int_push_back(l, 2);
+        TEST_CHECK(l != NULL);
+        TEST_CHECK(*(int*)l->next->data == 2);
+    
+        free_list(l);
 }
 
 void test_free()
 {
-    ERASE_INIT(int);
-    PUSH_FRONT_INIT(int);
-
-    list *l = NULL;
-    l = int_push_front(l , 1);
-    l = int_push_front(l , 2);
-    l = int_push_front(l , 3); 
-    // 3->2->1
-    l = int_erase(l, 1); // 3->2
-    TEST_CHECK(*(int*)l->data == 3);
-    TEST_CHECK(*(int*)l->next->data == 2);
-    l = int_erase(l, 3); // 2
-    TEST_CHECK(*(int*)l->data == 2);
-
-    free_list(l);
+        ERASE_INIT(int);
+        PUSH_FRONT_INIT(int);
+    
+        list *l = NULL;
+        l = int_push_front(l , 1);
+        l = int_push_front(l , 2);
+        l = int_push_front(l , 3); 
+        // 3->2->1
+        l = int_erase(l, 1); // 3->2
+        TEST_CHECK(*(int*)l->data == 3);
+        TEST_CHECK(*(int*)l->next->data == 2);
+        l = int_erase(l, 3); // 2
+        TEST_CHECK(*(int*)l->data == 2);
+    
+        free_list(l);
 }
 
 void test_size()
 {
-    list *l = NULL;
-    PUSH_FRONT_INIT(int);
+        list *l = NULL;
+        PUSH_FRONT_INIT(int);
 
-    l = int_push_front(l, 1);
-    l = int_push_front(l, 2);
-    l = int_push_front(l, 3);
+        l = int_push_front(l, 1);
+        l = int_push_front(l, 2);
+        l = int_push_front(l, 3);
 
-    TEST_CHECK(list_size(l) == 3);
+        TEST_CHECK(list_size(l) == 3);
     
-    free_list(l);
+        free_list(l);
 }
 
 void c_test_push()
 {
-    list *l = NULL;
+        list *l = NULL;
 
-    PUSH_FRONT_INIT(int);
-    C_PUSH_FRONT_INIT(int);
-    PUSH_BACK_INIT(int);
+        PUSH_FRONT_INIT(int);
+        C_PUSH_FRONT_INIT(int);
+        PUSH_BACK_INIT(int);
 
-    l = int_push_front(l , 1337);
-    TEST_CHECK(l != NULL);
-    l = int_push_front(l , 31337);
-    TEST_CHECK(l != NULL);
+        l = int_push_front(l , 1337);
+        TEST_CHECK(l != NULL);
+        l = int_push_front(l , 31337);
+        TEST_CHECK(l != NULL);
 
-    convert_to_circular(l);
+        convert_to_circular(l);
 
-    l = int_c_push_front(l , 1);
-    TEST_CHECK(l != NULL);
-    l = int_c_push_front(l , 2);
-    TEST_CHECK(l != NULL);
-    l = int_c_push_front(l , 3);
-    TEST_CHECK(l != NULL); 
+        l = int_c_push_front(l , 1);
+        TEST_CHECK(l != NULL);
+        l = int_c_push_front(l , 2);
+        TEST_CHECK(l != NULL);
+        l = int_c_push_front(l , 3);
+        TEST_CHECK(l != NULL); 
 
-    TEST_CHECK(*(int*)l->data == 3);
+        TEST_CHECK(*(int*)l->data == 3);
 
-    c_free_list(l);
+        c_free_list(l);
 }
 
 void c_test_free()
 {
-    list *l = NULL;
+        list *l = NULL;
 
-    C_ERASE_INIT(int);
-    PUSH_FRONT_INIT(int);
-    C_PUSH_FRONT_INIT(int);
-
-    l = int_push_front(l , 1337);
-    TEST_CHECK(l != NULL); 
-    l = int_push_front(l , 31337);
-    TEST_CHECK(l != NULL); 
-
-    convert_to_circular(l);
-
-    l = int_c_push_front(l , 1);
-    TEST_CHECK(l != NULL); 
-    l = int_c_push_front(l , 2);
-    TEST_CHECK(l != NULL); 
-    l = int_c_push_front(l , 3);
-    TEST_CHECK(l != NULL);  
-   
-    // (root)3->2->1->31337->1337->root
-
-    l = int_c_erase(l, 3); // (root)2->1->31337->1337->root
-    TEST_CHECK(*(int*)l->data == 2);
+        C_ERASE_INIT(int);
+        PUSH_FRONT_INIT(int);
+        C_PUSH_FRONT_INIT(int);
     
-    l = int_c_erase(l, 31337); // (root)2->1->1337->root
-    TEST_CHECK(*(int*)l->next->next->data == 1337);
+        l = int_push_front(l , 1337);
+        TEST_CHECK(l != NULL); 
+        l = int_push_front(l , 31337);
+        TEST_CHECK(l != NULL); 
+    
+        convert_to_circular(l);
+    
+        l = int_c_push_front(l , 1);
+        TEST_CHECK(l != NULL); 
+        l = int_c_push_front(l , 2);
+        TEST_CHECK(l != NULL); 
+        l = int_c_push_front(l , 3);
+        TEST_CHECK(l != NULL);  
+   
+        // (root)3->2->1->31337->1337->root
 
-    c_free_list(l);
+        l = int_c_erase(l, 3); // (root)2->1->31337->1337->root
+        TEST_CHECK(*(int*)l->data == 2);
+    
+        l = int_c_erase(l, 31337); // (root)2->1->1337->root
+        TEST_CHECK(*(int*)l->next->next->data == 1337);
+
+        c_free_list(l);
 }
 
 void c_test_size()
 {
-    list *l = NULL;
-    PUSH_FRONT_INIT(int);
-    C_PUSH_FRONT_INIT(int);
+        list *l = NULL;
+        PUSH_FRONT_INIT(int);
+        C_PUSH_FRONT_INIT(int);
 
-    l = int_push_front(l , 1337);
-    TEST_CHECK(l != NULL); 
-    l = int_push_front(l , 31337);
-    TEST_CHECK(l != NULL); 
+        l = int_push_front(l , 1337);
+        TEST_CHECK(l != NULL); 
+        l = int_push_front(l , 31337);
+        TEST_CHECK(l != NULL); 
 
-    convert_to_circular(l);
+        convert_to_circular(l);
 
-    l = int_c_push_front(l, 1);
-    l = int_c_push_front(l, 2);
-    l = int_c_push_front(l, 3);
-
-    TEST_CHECK(c_list_size(l) == 5);
+        l = int_c_push_front(l, 1);
+        l = int_c_push_front(l, 2);
+        l = int_c_push_front(l, 3);
     
-    c_free_list(l);
+        TEST_CHECK(c_list_size(l) == 5);
+    
+        c_free_list(l);
 }
 
 TEST_LIST = {
-    { "test_push", test_push },
-    { "test_free", test_free },
-    { "test_size", test_size },
-    { "c_test_push", c_test_push },
-    { "c_test_free", c_test_free },
-    { "c_test_size", c_test_size },
-    { NULL }
+        { "test_push", test_push },
+        { "test_free", test_free },
+        { "test_size", test_size },
+        { "c_test_push", c_test_push },
+        { "c_test_free", c_test_free },
+        { "c_test_size", c_test_size },
+        { NULL }
 };
