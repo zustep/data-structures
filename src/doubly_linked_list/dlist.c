7f8f625495d7b966378024b054492acc0645c2e9
Michael Sholkov <ecoaw1@gmail.com>
2021-04-30 14:03:00 +0300

    circular doubly ll

--- src/doubly_linked_list/dlist.c
+++ src/doubly_linked_list/dlist.c
@@ -7,18 +7,23 @@ int main(int argc, char *argv[])
 	DLIST_PUSH_FRONT_INIT(int);
 	DLIST_PUSH_BACK_INIT(int);
 	DLIST_ERASE_INIT(int);
+        CDLIST_PUSH_FRONT_INIT(int);
+        CDLIST_PUSH_BACK_INIT(int);
+        CDLIST_ERASE_INIT(int);
 	root = int_push_front(root, 1);
 	root = int_push_front(root, 2);
 	root = int_push_front(root, 3);
 	root = int_push_back(root, 1337);
-        printf("size = %d\n", dlist_size(root));
-	root = int_erase(root, 1337);
-	root = int_erase(root, 3);
-	root = int_erase(root, 2);
-        printf("size = %d\n", dlist_size(root));
-	dlist_int_display(root);
-        free_dlist(root);
-
+        convert_to_circular(root);
+        root = int_c_push_back(root, 1488);
+        root = int_c_push_front(root, 15555);
+        c_int_display(root);
+        root = int_c_erase(root, 1488);
+        c_int_display(root);
+        root = int_c_erase(root, 15555);
+        c_int_display(root);
+        root = int_c_erase(root, 2);
+        c_int_display(root);
 
 	return 0;
 }
@@ -54,4 +59,50 @@ unsigned int dlist_size(dlist *head)
         for (dlist *ptr = head; ptr; ptr = ptr->next)
                 count++;
         return count;
+}
+
+void convert_to_circular(dlist *head)
+{
+        dlist *ptr = head;
+        while (ptr->next != NULL) {
+                ptr = ptr->next;
+        }
+
+        head->prev = ptr;
+        ptr->next = head;
+}
+
+void c_int_display(dlist *head)
+{
+        dlist *ptr = head;
+        do {
+                printf("%d ", *(int*)ptr->data);
+                ptr = ptr->next;
+        } while (ptr != head);
+    
+    printf("\n");
+}
+
+
+void c_free_dlist(dlist *head)
+{
+        dlist *ptr = head;
+        do {
+                dlist *tmp = ptr;
+                ptr = ptr->next;
+                free(tmp->data);
+                free(tmp);
+        } while(ptr != head);
+}
+
+unsigned int c_dlist_size(dlist *head)
+{
+        dlist *ptr = head;
+        unsigned int count = 0;
+        do {
+                count++;
+                ptr = ptr->next;
+        } while(ptr != head);
+
+    return count;
 }
--- src/doubly_linked_list/dlist.h
+++ src/doubly_linked_list/dlist.h
@@ -10,11 +10,10 @@ typedef struct dlist
 }dlist;
 
 /* doubly linked list */
-
-void dlist_int_display(dlist *head);
+void dlist_int_display(dlist *head); // test
 unsigned int dlist_size(dlist *head);
 void free_dlist(dlist *head);
-void dlist_float_displyat(dlist *head);
+void dlist_float_display(dlist *head); // test
 
 #define DLIST_PUSH_FRONT_INIT(TYPE)				\
 	dlist* TYPE ##_push_front(dlist *head, TYPE value)	\
@@ -31,7 +30,8 @@ void dlist_float_displyat(dlist *head);
 								\
 		new_node->prev = NULL;				\
 		new_node->next = head;				\
-		head->prev = new_node;				\
+		head->prev = new_node;                          \
+                				                \
 		return new_node;				\
 	}							\
 
@@ -86,10 +86,98 @@ void dlist_float_displyat(dlist *head);
 		prev->next = ptr->next;				\
 		ptr->next->prev = prev;				\
 		free(ptr->data);				\
-		free(ptr);					\
+		free(ptr);                                      \
+                					        \
 		return head;					\
 	}							\
 /* doubly linked list */
 
+
+/* circular doubly linked list */
+void convert_to_circular(dlist *head);
+void c_free_dlist(dlist *head);
+unsigned int c_dlist_size(dlist *head);
+void c_int_display(dlist *head); // test
+
+#define CDLIST_PUSH_FRONT_INIT(TYPE)                            \
+        dlist* TYPE ##_c_push_front(dlist *head, TYPE value)    \
+        {                                                       \
+                dlist *new_node = malloc(sizeof(dlist));        \
+                new_node->data = malloc(sizeof(TYPE));          \
+                *(TYPE*)new_node->data = value;                 \
+                new_node->next = head;                          \
+                                                                \
+                dlist *ptr = head;                              \
+                while (ptr->next != head)                       \
+                        ptr = ptr->next;                        \
+                                                                \
+                new_node->prev = ptr;                           \
+                ptr->next = new_node;                           \
+                head->prev = new_node;                          \
+                                                                \
+                return new_node;                                \
+        }                                                       \
+
+#define CDLIST_PUSH_BACK_INIT(TYPE)                             \
+        dlist* TYPE ##_c_push_back(dlist *head, TYPE value)     \
+        {                                                       \
+                dlist *new_node = malloc(sizeof(dlist));        \
+                new_node->data = malloc(sizeof(TYPE));          \
+                *(TYPE*)new_node->data = value;                 \
+                new_node->next = head;                          \
+                                                                \
+                dlist *ptr = head;                              \
+                while (ptr->next != head)                       \
+                        ptr = ptr->next;                        \
+                                                                \
+                ptr->next = new_node;                           \
+                new_node->prev = ptr;                           \
+                head->prev = new_node;                          \
+                                                                \
+                return head;                                    \
+        }                                                       \
+
+#define CDLIST_ERASE_INIT(TYPE)                                 \
+        dlist * TYPE ##_c_erase(dlist *head, TYPE value)        \
+        {                                                       \
+                if (*(TYPE*)head->data == value) { 		\
+			dlist *ptr = head;                      \
+                        while (ptr->next != head)               \
+                                ptr = ptr->next;                \
+                                		                \
+			dlist *tmp = head;                      \
+                        ptr->next = head->next;		        \
+			head = head->next;			\
+                        head->prev = ptr;                       \
+			free(tmp->data);			\
+			free(tmp);                              \
+                        return head;				\
+		}						\
+								\
+		dlist *ptr = head;		                \
+		while (ptr->next != head)		        \
+			ptr = ptr->next;			\
+                                                                \
+                if (*(TYPE*)ptr->data == value) {               \
+                        ptr->prev->next = head;                 \
+                        head->prev = ptr;                       \
+                        free(ptr->data);                        \
+                        free(ptr);                              \
+                        return head;                            \
+                }                                               \
+								\
+		ptr = head;                                     \
+                while (*(TYPE*)ptr->data != value)              \
+                        ptr = ptr->next;                        \
+                                                                \
+                ptr->prev->next = ptr->next;                    \
+                ptr->next->prev = ptr->prev;                    \
+                free(ptr->data);                                \
+                free(ptr);                                      \
+                					        \
+		return head;					\
+                                                                \
+        }                                                       \
+/* circular doubly linked list */
 
 #endif
